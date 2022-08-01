14f4a2e74823e7c6b4f9c0de41a073ee7005c758
Victor Pinedo <victorpinedomartinez@gmail.com>
2022-07-31 21:43:51 -0500

    [add] coercion exercise

--- app.js
+++ app.js
@@ -1,3 +1,44 @@
-let value = 3;
+function isValidName(name) {
+  if (typeof name == "string" && name.trim().length >= 3) {
+    return true;
+  }
+  return false;
+}
 
-console.log(value.valueOf())
+function hoursAttended(attended, length) {
+  if (typeof attended == "string" && attended.trim() != "") {
+    attended = Number(attended);
+  }
+  if (typeof length == "string" && length.trim() != "") {
+    length = Number(length);
+  }
+  if (
+    typeof attended == "number" &&
+    typeof length == "number" &&
+    attended >= 0 &&
+    length >= 0 &&
+    Number.isInteger(attended) && 
+    Number.isInteger(length) && 
+    attended <= length
+  ) {
+    return true;
+  }
+  return false;
+}
+
+// test
+console.log(isValidName("Frank") === true);
+console.log(hoursAttended(6, 10) === true);
+console.log(hoursAttended(6, "10") === true);
+console.log(hoursAttended("6", 10) === true);
+console.log(hoursAttended("6", "10") === true);
+
+console.log(isValidName(false) === false);
+console.log(isValidName(null) === false);
+console.log(isValidName(undefined) === false);
+console.log(isValidName("") === false);
+console.log(isValidName("   \t\n") === false);
+console.log(isValidName("X") === false);
+console.log(hoursAttended("", 6) === false);
+console.log(hoursAttended(6, "") === false);
+console.log(hoursAttended("", "") === false);
